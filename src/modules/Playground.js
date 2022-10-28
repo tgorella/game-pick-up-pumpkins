50812639b2aa37fa26f983f82a36707b2974977b
tgorella <107557323+tgorella@users.noreply.github.com>
2022-10-28 13:17:15 +0300

    Update Playground.js

--- src/modules/Playground.js
+++ src/modules/Playground.js
@@ -1,7 +1,6 @@
+export function gameScreenHTML() {
 const scoreTable = document.createElement('div');
 scoreTable.id='score';
-
-export function gameScreenHTML() {
 const gameScreen = document.createElement('div');
 const screenTitle = document.createElement('h1');
 screenTitle.textContent = 'Pick Up Pumpkins';
@@ -9,6 +8,8 @@ const playgroundContainer = document.createElement('div');
 playgroundContainer.className='playground-container';
 const fenceSide = document.createElement('div');
 fenceSide.className = 'fence-side';
+const fenceSide1 = document.createElement('div');
+fenceSide1.className = 'fence-side';
 const playgroundMiddleSection = document.createElement('div');
 playgroundMiddleSection.id = 'playground';
 const fenceTop = document.createElement('div');
@@ -19,9 +20,7 @@ const fenceBottom = document.createElement('div');
 fenceBottom.className = 'fence-bottom';
 
 playgroundMiddleSection.append(fenceTop,playground, fenceBottom);
-playgroundContainer.append(fenceSide, playgroundMiddleSection, fenceSide);
+playgroundContainer.append(fenceSide1, playgroundMiddleSection, fenceSide);
 gameScreen.append(screenTitle, scoreTable, playgroundContainer);
 return gameScreen;
 }
-
-
